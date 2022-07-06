21dffc958364f6c711d07a9e8b82a89cc21af4aa
Mike An <mike.an@internet.ru>
2022-07-06 05:13:22 +0300

    Initial Commit

---
+++ ConsoleGraphics/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ConsoleGraphics
+//
+//  Created by Mike An on 06.07.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
